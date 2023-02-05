c29de51ff5167c2d2ea5bbfe3db90230523a6067
swiftGold <zloygold@gmail.com>
2023-02-05 19:56:12 +0300

    Initial commit

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var storyLabel: UILabel!
+    @IBOutlet var choice1Button: UIButton!
+    @IBOutlet var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        guard let title = sender.currentTitle else { fatalError() }
+        storyBrain.nextStory(userChoice: title)
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
 }
-
